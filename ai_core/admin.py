2884700fe34e80193e6e69d4f12141db77a8570f
Iago Santos Domínguex <iagocanalejas@gmail.com>
2021-07-15 19:08:11 +0200

    fix: traceable and auditable models not auto populating created_by fields

--- ai_core/admin.py
+++ ai_core/admin.py
@@ -11,11 +11,16 @@ class AuditedModelAdmin(StampedModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         return super(AuditedModelAdmin, self).get_readonly_fields(request, obj) + ('created_by',)
 
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.save()
+
 
 class TraceableModelAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         return super(TraceableModelAdmin, self).get_readonly_fields(request, obj) \
-               + ('from_date', 'to_date', 'is_active')
+               + ('from_date', 'to_date', 'is_active', 'created_by',)
 
     def response_change(self, request, obj):
         if "_unsubscribe" in request.POST:
@@ -23,3 +28,8 @@ class TraceableModelAdmin(admin.ModelAdmin):
             return HttpResponseRedirect(".")
 
         return super(TraceableModelAdmin, self).response_change(request, obj)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.save()
