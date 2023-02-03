b8eadbd5b38a4e9b8896530fc42bb2563578d85f
Iagocanalejas <iagocanalejas@gmail.com>
2023-02-03 23:22:08 +0100

    feat: add option to ignore quotes on remove_symbols function

--- ai_core/utils/strings.py
+++ ai_core/utils/strings.py
@@ -15,8 +15,9 @@ def remove_parenthesis(word: str) -> str:
     return whitespaces_clean(re.sub(r'\([\w\-_−–#: !+]*\)', '', word))
 
 
-def remove_symbols(word: str) -> str:
-    return whitespaces_clean(re.sub(r'[^a-zA-Z0-9\-]', ' ', word))
+def remove_symbols(word: str, ignore_quotes: bool = False) -> str:
+    reg = r'[^a-zA-Z0-9\-"\']' if ignore_quotes else r'[^a-zA-Z0-9\-]'
+    return whitespaces_clean(re.sub(reg, ' ', word))
 
 
 def remove_conjunctions(word: str) -> str:
