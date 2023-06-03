3354e675db62ffbd5af242ee47b0e7f13fe57951
Iagocanalejas <iagocanalejas@gmail.com>
2023-06-03 13:46:08 +0200

    feat: levenshtein_distance

--- ai_core/utils/strings.py
+++ ai_core/utils/strings.py
@@ -99,6 +99,30 @@ def find_date(w: str) -> Optional[date]:
 #                    UTILITIES                     #
 ####################################################
 
+def levenshtein_distance(s1, s2):
+    # Create a matrix with dimensions (len(s1) + 1) x (len(s2) + 1)
+    matrix = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+    # Initialize the first row and column of the matrix
+    for i in range(len(s1) + 1):
+        matrix[i][0] = i
+    for j in range(len(s2) + 1):
+        matrix[0][j] = j
+
+    # Compute the Levenshtein distance
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            cost = 0 if s1[i - 1] == s2[j - 1] else 1
+            matrix[i][j] = min(
+                matrix[i - 1][j] + 1,  # Deletion
+                matrix[i][j - 1] + 1,  # Insertion
+                matrix[i - 1][j - 1] + cost  # Substitution
+            )
+
+    # The Levenshtein distance is the value in the bottom-right cell of the matrix
+    return matrix[-1][-1]
+
+
 def closest_result(keyword: str, elements: List[str]) -> Tuple[Optional[str], float]:
     if any(e == keyword for e in elements):
         return keyword, 1.
